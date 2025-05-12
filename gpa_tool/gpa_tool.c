48cd452943ba6bf470acbb817235b635e3f4614f
Aleksandr Vorobiev <CynicRus@gmail.com>
2025-05-13 01:49:36 +0300

    Update gpa_tool.c

--- gpa_tool/gpa_tool.c
+++ gpa_tool/gpa_tool.c
@@ -1,6 +1,6 @@
 /*MIT License
 
-Copyright (c) 2025 Aleksand Vorobev aka CynicRus
+Copyright (c) 2025 Aleksandr Vorobev aka CynicRus
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
